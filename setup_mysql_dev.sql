679b71c7378246a41d5cb349c30455ddc8851442
Mishel450 <tomasmishel18@gmai.com>
2023-07-31 11:54:56 -0700

    task 2 database 1.2

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
 -- the sql
 CREATE DATABASE if NOT EXISTS hbnb_dev_db;
 CREATE USER if NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db` TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema` TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
