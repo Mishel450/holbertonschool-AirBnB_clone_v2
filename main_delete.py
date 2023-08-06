76bafbf8d8b212ea9bba228bebf4d2af6a1e619e
vicenzoanza <viceanza2010@hotmail.com>
2023-08-06 06:01:59 -0700

    tasks

--- main_delete.py
+++ main_delete.py
@@ -35,7 +35,8 @@ print("Another State: {}".format(another_state))
 # All States
 all_states = fs.all(State)
 print("All States: {}".format(len(all_states.keys())))
-for state_key in all_states.keys():
+
+for state_key in list(all_states.keys()):
     print(all_states[state_key])        
 
 # Delete the new State
