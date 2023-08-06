e6de3d48b49a745f779c5becf0fa5a3878daca99
vicenzoanza <viceanza2010@hotmail.com>
2023-08-06 06:08:56 -0700

    fix

--- models/amenity.py
+++ models/amenity.py
@@ -5,10 +5,16 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
 from os import getenv
+
+
 class Amenity(BaseModel, Base):
+
     """amenity class"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        place_amenities = relationship("Place", secondary=place_amenity, back_populates="amenities")
+    place_amenities = relationship(
+            "Place",
+            secondary=place_amenity,
+            back_populates="amenities"
+            )
