54977a3304d95e278eb6a31f5dedfdd2e0c482c8
Mishel450 <tomasmishel18@gmai.com>
2023-08-14 10:02:36 -0700

    0-hello_route.py done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""task-0"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def h_hbnb():
+    """returns Hello HBNB!"""
     return "Hello HBNB!"
 
 if __name__ == '__main__':
