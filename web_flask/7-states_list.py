d78188203c2e267e915bc22600b889692f802191
Mishel450 <tomasmishel18@gmai.com>
2023-08-16 05:53:27 -0700

    7-done all

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""task-7"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db():
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def st_lists():
+    from models.state import State
+    st_list = [] = storage.all(State).values()
+    return render_template('7.states_list.html', lista=st_list)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
